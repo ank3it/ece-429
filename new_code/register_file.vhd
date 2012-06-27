a7c6efdfb435ce0288fd8f2fdea55b7e2f203551
usmankhan9190@gmail.com <usmankhan9190@gmail.com@4ed65ab9-d869-6f9f-76b4-ac6694052e89>
2012-06-27 00:11:02 +0000

    Adding register_file.vhd. Verified basic functionality such as read/write, but still needs to be integrated into rest of code

---
+++ new_code/register_file.vhd
@@ -0,0 +1,46 @@
+-- register_file.vhd
+-- Group: 13
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity register_file is
+	generic(  
+		data_width 		: natural := 32;
+		addr_width 		: natural := 5;
+		size 			: natural := 32
+	);
+
+	port (
+		clock 			: in std_logic;
+		read_address1	: in std_logic_vector(addr_width - 1 downto 0);
+		read_address2	: in std_logic_vector(addr_width - 1 downto 0);
+		write_address	: in std_logic_vector(addr_width - 1 downto 0);
+		write_enable 	: in std_logic;
+		data_in			: in std_logic_vector(data_width - 1 downto 0);
+		data_out1 		: out std_logic_vector(data_width - 1 downto 0);
+		data_out2		: out std_logic_vector(data_width - 1 downto 0)
+	);
+end register_file;
+
+architecture main of register_file is        
+        type regfile_type is array (0 to size - 1) of std_logic_vector(data_width - 1 downto 0);
+        signal regfile : regfile_type ;
+begin
+
+	RW : process
+	begin
+		wait until rising_edge(clock);
+		-- Read
+		data_out1 <= regfile(to_integer(unsigned(read_address1)));
+		data_out2 <= regfile(to_integer(unsigned(read_address2)));
+		
+		-- Write
+		if write_enable = '1' then
+			regfile(to_integer(unsigned(write_address))) <= data_in;
+		end if;
+	end process;
+
+end main;
