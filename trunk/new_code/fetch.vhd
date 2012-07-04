f06bc39ea85c3a0a202f655f6796c1759a0a1748
ayushgupta1991@gmail.com <ayushgupta1991@gmail.com@4ed65ab9-d869-6f9f-76b4-ac6694052e89>
2012-07-04 20:43:22 +0000

    fixed fetch counter

--- trunk/new_code/fetch.vhd
+++ trunk/new_code/fetch.vhd
@@ -32,7 +32,7 @@ architecture main of fetch is
      fetch2:process
         variable my_line : line;  -- type 'line' comes from textio
         variable addressword : unsigned(31 downto 0) := x"00000000";
-        variable counter : integer := 50;
+        variable counter : integer := 130;
       begin
         wait until rising_edge(clk);
         if reset = '0' then
