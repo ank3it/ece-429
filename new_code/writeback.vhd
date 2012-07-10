e0322f5bcf509d0243273f5c9485dbdf6f894d2f
z2alam90@gmail.com <z2alam90@gmail.com@4ed65ab9-d869-6f9f-76b4-ac6694052e89>
2012-07-10 03:31:00 +0000

    writeback logic

--- new_code/writeback.vhd
+++ new_code/writeback.vhd
@@ -4,12 +4,15 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
 
 entity writeback is
 	port (
 		-- Inputs
-		clk					: in std_logic;
-		i_stall				: in std_logic;
+		clk			: in std_logic;
+		i_stall			: in std_logic;
 		i_control_signal	: in std_logic_vector(6 downto 0);
 		i_execute_output	: in std_logic_vector(31 downto 0);
 		i_memory_output		: in std_logic_vector(31 downto 0);
@@ -17,20 +20,22 @@ entity writeback is
 		-- Outputs
 		o_write_enable		: out std_logic;
 		o_write_address		: out std_logic_vector(4 downto 0);
-		o_data				: out std_logic_vector(31 downto 0)
+		o_data			: out std_logic_vector(31 downto 0)
 	);
 end writeback;
 
 architecture main of writeback is
-	signal write_address	: std_logic_vector(4 downto 0);
-	signal data				: std_logic_vector(31 downto 0);
+	signal write_address		: std_logic_vector(4 downto 0);
+	signal data			: std_logic_vector(31 downto 0);
 	signal wb_select		: std_logic;
 	signal wb_disable		: std_logic;
+	signal write_enable		: std_logic;
 begin
 
-	process
+process
 	begin
 		wait until rising_edge(clk);
+		o_write_enable <= write_enable;
 		o_write_address <= write_address;
 		o_data <= data;
 	end process;
@@ -41,5 +46,21 @@ begin
 	wb_disable <= i_control_signal(6);
 	
 	-- Writeback logic here
+	write_enable <= '1' when (wb_disable = '0') else '0';
+	data <= i_execute_output when (wb_select = '0') else i_memory_output;
+
+
+	--if (wb_disable = '0') then
+	--	write_enable <= '1';
+	--	
+	--	if wb_select = '0' then -- output from execute stage
+	--		data <= i_execute_output;
+	--	else 	
+	--		data <= i_memory_output;
+	--	end if;	
+	--else
+	--	write_enable <= '0';
+	--end if;
+		
 
 end main;
